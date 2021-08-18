c5fff84970525dd9dcdfc4f1e21dce3c84543495
stack72 <public@paulstack.co.uk>
2021-08-18 13:00:51 +0300

    Upgrade Examples to use the new Akamai account

--- examples/edge-hostname/python/__main__.py
+++ examples/edge-hostname/python/__main__.py
@@ -3,14 +3,14 @@
 import pulumi
 import pulumi_akamai as akamai
 
-group_id = "grp_175145"
-product_id = "prd_Adaptive_Media_Delivery"
+group_id = "grp_201328"
+product_id = "prd_Fresca"
 
 contract_id = pulumi.Output.from_input(akamai.get_contract(group_id = group_id)).id
 
 pydomain = akamai.EdgeHostName("test",
                                   contract_id=contract_id,
                                   group_id=group_id,
-                                  product_id="prd_Adaptive_Media_Delivery",
-                                  edge_hostname="test-py.mycompany",
+                                  product_id=product_id,
+                                  edge_hostname="test-py.pulumi",
                                   ip_behavior="ipv4")
